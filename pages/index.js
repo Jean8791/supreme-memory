c3a49e99e5300a054d2013af6556b6189e58731b
jean <jeanleonardocdr2209@gmail.com>
2025-12-03 17:08:51 -0300

    adiciona arquivo ´.editorconfig´

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 function Home() {
-    return <h1>Colecione memórias e acumule sorrisos. Todo o resto é passageiro. 😊</h1>
+  return <h1>Colecione memórias e acumule sorrisos. Todo o resto é passageiro. 😊</h1>
 }
+
 export default Home;
