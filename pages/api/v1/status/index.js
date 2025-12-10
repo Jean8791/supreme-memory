ee136270347e789c05091097d941f73f4db58281
jean <jeanleonardocdr2209@gmail.com>
2025-12-10 14:33:27 -0300

    move `.env` file to `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,8 @@ import database from '../../../../infra/database.js';
 
 async function index(req, res) {
   const result = await database.query('SELECT NOW()');
-  console.log(result);
-  res.status(200).json({status: 'ok'});
+  console.log(result.rows);
+  res.status(200).json({status: result.rows});
 }
 
 export default index;
