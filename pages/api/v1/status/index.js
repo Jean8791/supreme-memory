287f144276820c4115b59ece53412ebaa0cd0f68
jean <jeanleonardocdr2209@gmail.com>
2025-12-11 23:13:25 -0300

    finish  andpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
-import database from 'infra/database.js';
+import database from "infra/database";
 
-async function index(req, res) {
-  const result = await database.query('SELECT NOW()');
-  console.log(result.rows);
-  res.status(200).json({status: result.rows});
+async function status(req, res) {
+  const updatedAt = new Date().toISOString();
+
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version
+
+  const dataBaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dataBaseMaxConnectionsValue = dataBaseMaxConnectionsResult.rows[0].max_connections
+
+  const dataBaseName = process.env.POSTGRES_DB;
+
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname =$1;",
+    values: [dataBaseName],
+  });
+
+  const dataBaseOpenedConnectionsValue = dataBaseOpenedConnectionsResult.rows[0].count || 0;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: dataBaseOpenedConnectionsValue
+      }
+    }
+  });
 }
 
-export default index;
+export default status;
