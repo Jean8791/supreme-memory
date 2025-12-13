be1545cda22806b62851db92a85d07a2f5f3ac18
jean <jeanleonardocdr2209@gmail.com>
2025-12-13 17:43:58 -0300

    adicionando a config do ssl

--- infra/database.js
+++ infra/database.js
@@ -8,9 +8,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    ssl: process.env.NODE_ENV !== "development",
   });
 
   await client.connect();
